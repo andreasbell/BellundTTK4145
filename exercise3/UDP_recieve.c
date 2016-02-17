028102765b3fb9e3031a6124a4ef7eec789341dd
Student <student@itk-d650>
2016-02-17 11:27:16 +0100

    Exercise 6 done

--- exercise3/UDP_recieve.c
+++ exercise3/UDP_recieve.c
@@ -1,10 +1,11 @@
 /************* UDP CLIENT CODE *******************/
-//clear; clang-3.6 -Weverything UDP_receive.c -o test
+//clear; clang-3.6 -Weverything UDP_recieve.c -o test
 
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <errno.h>
 
 int main(){
   int Socket, portNum, nBytes;
@@ -12,11 +13,11 @@ int main(){
   struct sockaddr_in serverAddr;
 
   /*Create UDP socket*/
-  Socket = socket(PF_INET, SOCK_DGRAM , 0);
+  Socket = socket(PF_INET, SOCK_DGRAM | SOCK_NONBLOCK, 0);
 
   /*Configure settings in address struct*/
   serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(30016);
+  serverAddr.sin_port = htons(20016);
   serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
   bind(Socket, (struct sockaddr *)&serverAddr, sizeof serverAddr);
@@ -24,9 +25,14 @@ int main(){
   while(1){
     nBytes = recv(Socket,buffer,1024,0);
     if (nBytes > 0){
-    	printf("Received from server: %s\n",buffer);
+    	printf("Received from server: %i, %s\n", nBytes, buffer);
 	nBytes = 0;
     }
+    /*if (nBytes < 0){
+	printf("ERROR: %s\n", strerror(errno));
+	close(Socket);
+	return 1;
+    }*/
   }
 
   return 0;
