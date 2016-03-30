781bf940f9ea6ec47cfafb1c5e19a012311a3b0c
emilandreas <emilandreaslund@gmail.com>
2016-03-30 08:49:28 +0200

    pulling after japan

--- exercise5/message_manager.cpp
+++ exercise5/message_manager.cpp
@@ -1,8 +1,9 @@
 #include "message_manager.h"
+#include "Elevator.h"
 
 /*message format
  * {
- *  "que":[1,2,3,4],
+ *  "que":[{*floor*,*order_type*},2,3,4],
  *  "dir": 1,
  *  "lastfloor": 1,
  *  "current_state": "RUN"
@@ -12,14 +13,18 @@
 std::string make_message(Elevator elev){
     Json::Value message;
     Json::Value order;
-    for(int i = 0, i = elev.que.size(){
-        order[
-        message["que"][i] = elev.que;
+    for(int i = 0; i < elev.que.size(); i++){
+        message["que"][i] = {elev.que[i].floor, elev.que[i].type};
     }
 } 
 Elevator unpack_message(std::string message){
-    
+    return; 
 }
 int main(){
+    Elevator elev;
+    elev.current_state = RUN;
+    elev.direction = 1;
+    elev.last_floor = 1;
+    elev.que.get
     return 0;
 }
