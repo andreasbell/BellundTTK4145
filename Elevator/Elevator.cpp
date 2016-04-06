9e69571e159992bd753c1a81c2eb2ca778623391
blablabla <blablabla>
2016-04-06 17:05:30 +0200

    changes made

--- Elevator/Elevator.cpp
+++ Elevator/Elevator.cpp
@@ -147,7 +147,7 @@ int Elevator::next_stop(){
 		}
 	}
 
-	int loop_position = (direction == DIRN_UP)? last_floor + direction: last_floor + direction + N_FLOORS;
+	int loop_position = (direction == DIRN_UP)? last_floor + direction: 2*N_FLOORS - last_floor - direction;
 	for(int count = 0; count < 2*N_FLOORS; count++){
 		int floor = ((count + loop_position)/N_FLOORS%2)? N_FLOORS -1 - (count + loop_position)%N_FLOORS : (count + loop_position)%N_FLOORS;
 		int type = (count + loop_position)/N_FLOORS%2;
